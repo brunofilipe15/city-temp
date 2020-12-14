47fa0cb5e30d2ceede4bf5b35951139e0dcc8a37
Bruno <bruno.fernandes@airc.pt>
2020-12-14 18:04:19 +0000

    Backend init

---
+++ backend/citytemp/src/main/java/pt/brunofilipe/citytemp/CitytempApplication.java
@@ -0,0 +1,16 @@
+package pt.brunofilipe.citytemp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@SpringBootApplication
+public class CitytempApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitytempApplication.class, args);
+	}
+
+
+}
---
+++ backend/citytemp/src/test/java/pt/brunofilipe/citytemp/CitytempApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.brunofilipe.citytemp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitytempApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
