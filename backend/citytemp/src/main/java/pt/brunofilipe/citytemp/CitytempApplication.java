9ae8c8133f50dc069e1c6b83a8d3c35857ff6da0
Bruno <bruno.fernandes@airc.pt>
2020-12-14 20:55:37 +0000

    H2 database

--- backend/citytemp/src/main/java/pt/brunofilipe/citytemp/CitytempApplication.java
+++ backend/citytemp/src/main/java/pt/brunofilipe/citytemp/CitytempApplication.java
@@ -1,9 +1,14 @@
 package pt.brunofilipe.citytemp;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import pt.brunofilipe.citytemp.dao.CityRepository;
+import pt.brunofilipe.citytemp.model.City;
 
 @SpringBootApplication
 public class CitytempApplication {
@@ -12,5 +17,19 @@ public class CitytempApplication {
 		SpringApplication.run(CitytempApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner citiesData(CityRepository cityRepository) {
+		return args -> {
+			cityRepository.save(City.builder().name("Coimbra").build());
+			cityRepository.save(City.builder().name("Braga").build());
+			cityRepository.save(City.builder().name("Mirandela").build());
+			cityRepository.save(City.builder().name("Guarda").build());
+			cityRepository.save(City.builder().name("Sintra").build());
+			cityRepository.save(City.builder().name("Aveiro").build());
+			cityRepository.save(City.builder().name("Almada").build());
+			cityRepository.save(City.builder().name("Águeda").build());
+			cityRepository.save(City.builder().name("Gondomar").build());
+		};
+	}
 
 }
---
+++ backend/citytemp/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+CREATE TABLE cities (
+  id INT AUTO_INCREMENT  PRIMARY KEY,
+  name VARCHAR(250) NOT NULL
+);
+INSERT INTO cities (name) VALUES ('Aliko'),  ('Bill'),  ('Folrunsho');
