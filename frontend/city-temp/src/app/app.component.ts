93a1efee1cb5f5b3985f055d61715dd1d07d3da9
Bruno <bruno.fernandes@airc.pt>
2020-12-17 00:08:26 +0000

    clean frontend

--- frontend/city-temp/src/app/app.component.ts
+++ frontend/city-temp/src/app/app.component.ts
@@ -47,11 +47,11 @@ export class AppComponent implements OnInit {
   temperatures: Temperature[];
   chartReady = false;
   numberOfDaysOptions = [
-    {label: '1', value: 1},
-    {label: '2', value: 2},
-    {label: '3', value: 3},
-    {label: '4', value: 4},
-    {label: '5', value: 5}
+    { label: '1', value: 1 },
+    { label: '2', value: 2 },
+    { label: '3', value: 3 },
+    { label: '4', value: 4 },
+    { label: '5', value: 5 }
   ];
   numberOfDaysSelect: SelectItem;
   numberOfDays: number;
@@ -60,156 +60,167 @@ export class AppComponent implements OnInit {
   city: City;
 
   unitOfTemperature = [
-    {name: 'Celsius', value: unitOfTemperatureEnum.CELSIUS},
-    {name: 'Fahrenheit', value: unitOfTemperatureEnum.FAHRENHEIT}
+    { name: 'Celsius', value: unitOfTemperatureEnum.CELSIUS },
+    { name: 'Fahrenheit', value: unitOfTemperatureEnum.FAHRENHEIT }
   ];
   unitOfTemperatureSelect: any;
 
   temperatureNow: Temperature;
   UnitOfTemperatureEnum = unitOfTemperatureEnum;
 
+  //Chart Data
+  temperatureCelsius = [];
+  temperatureFahrenheit = [];
+  dates = [];
+
   constructor(private apiService: ApiService,
     private messageService: MessageService) { }
 
-  ngOnInit() { 
+  ngOnInit() {
     this.apiService.getCities().subscribe(cities => {
       cities.forEach(city => {
-        this.citiesOptions.push({label: city.name, value: city});
+        this.citiesOptions.push({ label: city.name, value: city });
       });
-      
+
       this.unitOfTemperatureSelect = unitOfTemperatureEnum.CELSIUS;
       this.city = this.citiesOptions[0].value;
       this.numberOfDays = this.numberOfDaysOptions[0].value;
-      
+
       this.getTemperatureNow(this.city);
-      this.createCharts(this.numberOfDays, this.city.id, this.unitOfTemperatureSelect);
-    }, error => { this.messageService.add({ severity: 'error', summary: 'Error', detail: error ? 'Service Unavailable' : error.error.message })});
+      this.getDataTemperatures();
+
+    }, error => { this.messageService.add({ severity: 'error', summary: 'Error', detail: error ? 'Service Unavailable' : error.error.message }) });
 
   }
 
   onChangeNumberOfDays(event) {
     this.numberOfDays = event.value;
-    this.createCharts(this.numberOfDays, this.city.id, this.unitOfTemperatureSelect);
+    this.getDataTemperatures();
   }
-  
+
   onChangeCity(event) {
     this.city = event.value;
     this.getTemperatureNow(this.city);
-    this.createCharts(this.numberOfDays, this.city.id, this.unitOfTemperatureSelect);
+    this.getDataTemperatures();
   }
 
   onChangeUnitOfTemperature(event) {
     this.unitOfTemperatureSelect = event.value;
-    this.createCharts(this.numberOfDays, this.city.id, this.unitOfTemperatureSelect);
+    this.createCharts(this.unitOfTemperatureSelect);
   }
 
-  createCharts(numberOfDays: number, cityId: number, unitOfTemperature: unitOfTemperatureEnum) {
-    this.chartReady = false;
-    let temperatureCelsius = [];
-    let temperatureFahrenheit = [];
-    let dates = [];
-    
-    this.apiService.getTemperatures(numberOfDays, cityId).subscribe(temperatures => {
+  getDataTemperatures() {
+    this.dates = [];
+    this.temperatureCelsius = [];
+    this.temperatureFahrenheit = [];
+    this.apiService.getTemperatures(this.numberOfDays, this.city.id).subscribe(temperatures => {
       this.temperatures = temperatures;
       this.temperatures.forEach(temperature => {
         let date = temperature.localDate;
-        dates.push(date);
-        temperatureCelsius.push([date, Math.round(temperature.tempCelsius * 10) / 10]);
-        temperatureFahrenheit.push([date,  Math.round(temperature.tempFahrenheit * 10) / 10]);
+        this.dates.push(date);
+        this.temperatureCelsius.push([date, this.roundNumber(temperature.tempCelsius)]);
+        this.temperatureFahrenheit.push([date, this.roundNumber(temperature.tempFahrenheit)]);
       });
-  
-      this.chartBig = {
-        series: [
-          {
-            name: "Temperature",
-            data: unitOfTemperature === unitOfTemperatureEnum.CELSIUS ? temperatureCelsius : temperatureFahrenheit,
-            color: "#556CBA"
-          },
-        ],
-        chart: {
-          id: "chartBig",
-          type: "line",
-          height: 230,
-          toolbar: {
-            autoSelected: "pan",
-            show: false
-          }
+      this.createCharts(this.unitOfTemperatureSelect);
+    }), (error => {
+      this.messageService.add({ severity: 'error', summary: 'Error', detail: error ? 'Service Unavailable' : error.error.message });
+    });
+  }
+
+  createCharts(unitOfTemperature: unitOfTemperatureEnum) {
+    this.chartReady = false;
+    this.chartBig = {
+      series: [
+        {
+          name: "Temperature",
+          data: unitOfTemperature === unitOfTemperatureEnum.CELSIUS ? this.temperatureCelsius : this.temperatureFahrenheit,
+          color: "#556CBA"
         },
-        colors: ["#546E7A"],
-        stroke: {
-          curve: 'smooth',
-          width: 3
-        },
-        fill: {
-          opacity: 1
-        },
-        markers: {
-          size: 0
-        },
-        xaxis: {
-          type: "datetime"
-        }
-      };
-  
-      this.chartSmall = {
-        series: [
-          {
-            name: "Temperature",
-            data: unitOfTemperature === unitOfTemperatureEnum.CELSIUS ? temperatureCelsius : temperatureFahrenheit,
-            color: "#556CBA"
-          },
-        ],
-        chart: {
-          id: "chartSmall",
-          height: 130,
-          type: "area",
-          brush: {
-            target: "chartBig",
-            enabled: true
-          },
-          selection: {
-            enabled: true,
-            xaxis: {
-              min: new Date(dates[0]).getTime(),
-              max: new Date(dates[dates.length - 1]).getTime()
-            }
-          }
-        },
-        colors: ["#008FFB"],
-        fill: {
-          type: 'gradient',
-          gradient: {
-            opacityFrom: 1,
-            opacityTo: 0.1,
-          },
-        },
-        xaxis: {
-          type: "datetime",
-          tooltip: {
-            enabled: false
-          }
-        },
-        yaxis: {
-          tickAmount: 2
-        },
-        legend: {
+      ],
+      chart: {
+        id: "chartBig",
+        type: "line",
+        height: 230,
+        toolbar: {
+          autoSelected: "pan",
           show: false
         }
-      };
-      this.chartReady = true;
-    },
-    error => { this.messageService.add({ severity: 'error', summary: 'Error', detail: error ? 'Service Unavailable' : error.error.message })});      
+      },
+      colors: ["#546E7A"],
+      stroke: {
+        curve: 'smooth',
+        width: 3
+      },
+      fill: {
+        opacity: 1
+      },
+      markers: {
+        size: 0
+      },
+      xaxis: {
+        type: "datetime"
+      }
+    };
+
+    this.chartSmall = {
+      series: [
+        {
+          name: "Temperature",
+          data: unitOfTemperature === unitOfTemperatureEnum.CELSIUS ? this.temperatureCelsius : this.temperatureFahrenheit,
+          color: "#556CBA"
+        },
+      ],
+      chart: {
+        id: "chartSmall",
+        height: 130,
+        type: "area",
+        brush: {
+          target: "chartBig",
+          enabled: true
+        },
+        selection: {
+          enabled: true,
+          xaxis: {
+            min: new Date(this.dates[0]).getTime(),
+            max: new Date(this.dates[this.dates.length - 1]).getTime()
+          }
+        }
+      },
+      colors: ["#008FFB"],
+      fill: {
+        type: 'gradient',
+        gradient: {
+          opacityFrom: 1,
+          opacityTo: 0.1,
+        },
+      },
+      xaxis: {
+        type: "datetime",
+        tooltip: {
+          enabled: false
+        }
+      },
+      yaxis: {
+        tickAmount: 2
+      },
+      legend: {
+        show: false
+      }
+    };
+    this.chartReady = true;
   }
 
   getTemperatureNow(city: City) {
     this.apiService.getNowTemperature(city.id).subscribe(temperatureNow => {
-        this.temperatureNow = temperatureNow;
-        this.temperatureNow.tempFahrenheit = Math.round(temperatureNow.tempFahrenheit * 10) / 10;
-        this.temperatureNow.tempCelsius = Math.round(temperatureNow.tempCelsius * 10) / 10;
-      },
-    error => { this.messageService.add({ severity: 'error', summary: 'Error', detail: error ? 'Service Unavailable' : error.error.message })});    
+      this.temperatureNow = temperatureNow;
+      this.temperatureNow.tempFahrenheit = this.roundNumber(temperatureNow.tempFahrenheit);
+      this.temperatureNow.tempCelsius = this.roundNumber(temperatureNow.tempCelsius);
+    },
+      error => { this.messageService.add({ severity: 'error', summary: 'Error', detail: error ? 'Service Unavailable' : error.error.message }) });
+  }
+
+  roundNumber(number: number): number {
+    return Math.round(number * 10) / 10;
   }
 
 }
-
-
